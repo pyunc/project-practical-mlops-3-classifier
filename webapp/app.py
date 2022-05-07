ad847ee7b6b89909a7207f2b1ec849abf58e643f
paulo yun <paulo.yun@ifood.com.br>
2022-05-07 13:39:20 +0100

    app creation

--- webapp/app.py
+++ webapp/app.py
@@ -1,61 +1,62 @@
-from flask import Flask, request, jsonify
-import pandas as pd
-import joblib
-from sklearn.preprocessing import StandardScaler
+from concurrent.futures import thread
+from flask import Flask, request, jsonify, send_from_directory, render_template
+from keras.models import load_model
+from keras.preprocessing.image import img_to_array, load_img
+import tensorflow as tf
+import os
+from werkzeug.utils import secure_filename
 
+import logging
+log = logging.getLogger(__name__)
 app = Flask(__name__)
 
-def scale(payload):
-    scaler = StandardScaler().fit(payload)
-    return scaler.transform(payload)
+IMAGE_SIZE = (180, 180)
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route('/', methods=['GET'])
+
+@app.route('/')
 def home():
-    return "<h3>Sklearn Prediction Container</h3>"
+    # return render_template('home.html', label='', imagesource='file://null')
+    return render_template('home.html', label='', imagesource='')
 
-@app.route("/predict", methods=['POST', 'GET'])
-def predict():
-    
-    """
-    Input sample:
-    {
-    "CHAS": { "0": 0 }, "RM": { "0": 6.575 },
-    "TAX": { "0": 296 }, "PTRATIO": { "0": 15.3 },
-    "B": { "0": 396.9 }, "LSTAT": { "0": 4.98 }
-    }
-    Output sample:
-    { "prediction": [ 20.35373177134412 ] }
-    """
 
-    if request.method=="POST":
-        clf = joblib.load("boston_housing_prediction.joblib")
-        inference_payload = pd.DataFrame(request.json)
-        
-    
-    if request.method=="GET":
-        args = request.args
-        CHAS = args.get("CHAS", default=0, type=int)
-        RM = args.get("RM", default=0, type=float)
-        TAX = args.get("TAX", default=0, type=float)
-        PTRATIO = args.get("PTRATIO", default=0, type=float)
-        B = args.get("B", default=0, type=float)
-        LSTAT = args.get("LSTAT", default=0, type=float)
+def predict(file):
 
-        input = ({
-        "CHAS": { "0": f"{CHAS}" },
-        "RM": { "0": f"{RM}" },
-        "TAX": { "0": f"{TAX}" },
-        "PTRATIO": { "0": f"{PTRATIO}" },
-        "B": { "0": f"{B}" },
-        "LSTAT": { "0": f"{LSTAT}" },
-        })
+    img = load_img(file, target_size=IMAGE_SIZE)
 
-        clf = joblib.load("boston_housing_prediction.joblib")
-        inference_payload = pd.DataFrame(input)
-    
-    scaled_payload = scale(inference_payload)
-    prediction = list(clf.predict(scaled_payload))
-    return jsonify({'prediction': prediction})
+    img_array = img_to_array(img)
+    img_array = tf.expand_dims(img_array, 0)  # Create batch axis
+    loaded_model = load_model('model_pet.h5')
+    predictions = loaded_model.predict(img_array)
+
+    score = predictions[0]
+
+    # return {'prediction': score}
+    return "This image is %.2f percent cat and %.2f percent dog." % (100 * (1 - score), 100 * score)
+
+
+@app.route("/", methods=['POST', 'GET'])
+def upload_file():
+
+    if request.method == 'POST':
+        file = request.files['file']
+        filename = secure_filename(file.filename)
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(file_path)
+        output = predict(file_path)
+
+        return render_template(
+            "home.html",
+            label=output,
+            imagesource=file_path)
+
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(debug=True, threaded=False)
